cf8ff01432ba908bd967e0823d22d76f8f4b3bcb
土豆 <a0077_2024@qq.com>
2025-02-24 20:34:54 +0800

    Update CounterManager.java
    2.24

--- springboot-init-master/src/main/java/com/tudou/tudoumianshi/manager/CounterManager.java
+++ springboot-init-master/src/main/java/com/tudou/tudoumianshi/manager/CounterManager.java
@@ -1,39 +1,49 @@
 package com.tudou.tudoumianshi.manager;
+import cn.dev33.satoken.fun.SaFunction;
 import cn.hutool.core.util.StrUtil;
 import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import com.alibaba.nacos.api.config.annotation.NacosValue;
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
 import com.tudou.tudoumianshi.utils.DateUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RAtomicLong;
+import org.redisson.api.RScript;
 import org.redisson.api.RedissonClient;
+import org.redisson.client.codec.IntegerCodec;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+import redis.clients.jedis.Jedis;
+
 import javax.annotation.Resource;
 import java.time.Instant;
-import java.util.Date;
+import java.util.*;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.atomic.LongAdder;
 @Slf4j
 @Service
 public class CounterManager {
 
-    @Resource
+
+     @Resource
     private RedissonClient redissonClient;
 
     // 使用LongAdder作为高效计数器
-    private static final LongAdder requestCounter = new LongAdder();
+    //private static final LongAdder requestCounter = new LongAdder();
 
     //@NacosValue(value = "${timeInterval}", autoRefreshed = true)
-    private Integer timeInterval;
-    @Resource
-    private RedisTemplate redisTemplate;
-
-//    private final Cache<String, Integer> answerCacheMap =
-//            Caffeine.newBuilder().initialCapacity(1024)
-//                    // 缓存180秒移除
-//                    .expireAfterAccess(60L, TimeUnit.SECONDS)
-//                    .build();
 
 
+    private Cache<String, Integer> counterCache = Caffeine.newBuilder()
+            .expireAfterWrite(1, TimeUnit.MINUTES)
+            .build();
+
+    // 定时任务执行间隔（毫秒），这里设置为 2 秒
+    private static final long INTERVAL = 2 * 1000;
 
     /**
      * 增加并返回计数，默认统计一分钟内的计数结果
@@ -44,11 +54,11 @@ public class CounterManager {
     public long incrAndGetCounter(String key) {
         return incrAndGetCounter(key, 1, TimeUnit.MINUTES);
     }
-    public long handleBlock() {
-        requestCounter.increment();
-        // 降级处理逻辑
-        return requestCounter.longValue();
-    }
+//    public long handleBlock() {
+//        requestCounter.increment();
+//        // 降级处理逻辑
+//        return requestCounter.longValue();
+//    }
 
     /**
      * 增加并返回计数
@@ -126,33 +136,23 @@ public class CounterManager {
 //                RScript.ReturnType.INTEGER,
 //                Collections.singletonList(key), expirationTimeInSeconds);
 //        return (long) countObj;
-//        Integer count = answerCacheMap.getIfPresent(redisKey);
-//        if (count == null) {
-//            answerCacheMap.put(redisKey, 0);
-//        }
-//        count = answerCacheMap.getIfPresent(redisKey);
-//        count+=1;
-//        answerCacheMap.put(redisKey,count);
-        requestCounter.increment();
+//
+        counterCache.asMap().compute(redisKey, (k, v) -> {
+            return v == null ? 1 : v + 1;
+        });
+        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+        scheduler.scheduleAtFixedRate(() -> {
+            Integer count = counterCache.asMap().get(redisKey);
+            RAtomicLong atomicLong = redissonClient.getAtomicLong(redisKey);
+            if (atomicLong == null) {
+                atomicLong.addAndGet(count);
+                atomicLong.expire(1, TimeUnit.MINUTES);
+            } else {
+                atomicLong.addAndGet(count);
+            }
+            counterCache.invalidateAll();
+        }, 0, 5, TimeUnit.SECONDS);
 
-        Date lastTime = new Date(System.currentTimeMillis() - 5000); // 设置为5秒前的时间
-
-        // 获取当前时间
-        Date currentTime = new Date();
-        // 使用DateUtil工具类检查当前时间与上一段时间是否超过5秒
-        int count_=0;
-        if (DateUtil.secondsBetween(lastTime, currentTime) > 2) {
-            // 保存计数器到 Redis
-            count_=saveByRedis(redisKey, requestCounter.intValue(), expirationTimeInSeconds);
-        }
-        return count_;
-    }
-
-    private int saveByRedis(String key, Integer count, int expirationTimeInSeconds) {
-        if(count==null){
-            return 0;
-        }
-        redisTemplate.opsForValue().set(key,count,expirationTimeInSeconds, TimeUnit.SECONDS);
-        return (int) redisTemplate.opsForValue().get(key);
+        return redissonClient.getAtomicLong(redisKey).get();
     }
 }
