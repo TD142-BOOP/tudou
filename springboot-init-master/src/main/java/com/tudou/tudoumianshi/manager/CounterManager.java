ef86f9d48b14b0bde670b3357666dc7ab0e14daa
土豆 <a0077_2024@qq.com>
2025-03-03 15:07:51 +0800

    Update CounterManager.java
    3.3

--- springboot-init-master/src/main/java/com/tudou/tudoumianshi/manager/CounterManager.java
+++ springboot-init-master/src/main/java/com/tudou/tudoumianshi/manager/CounterManager.java
@@ -8,6 +8,9 @@ import org.redisson.api.RScript;
 import org.redisson.api.RedissonClient;
 import org.redisson.client.codec.IntegerCodec;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import javax.annotation.Resource;
 import java.time.Instant;
 import java.util.*;
@@ -20,6 +23,23 @@ public class CounterManager {
     @Resource
     private RedissonClient redissonClient;
 
+    private static final String LUA_SCRIPT =
+            "if redis.call('exists', KEYS[1]) == 1 then " +
+                    "  redis.call('set', KEYS[1], ARGV[1]); " +
+                    "else " +
+                    "  redis.call('set', KEYS[1], ARGV[1], 'EX', ARGV[2]); " +
+                    "end " +
+                    "return ARGV[1];";
+
+    private static final ThreadPoolExecutor customExecutor = new ThreadPoolExecutor(
+            5,             // 核心线程数
+            10,                        // 最大线程数
+            60L,                       // 线程空闲存活时间
+            TimeUnit.SECONDS,           // 存活时间单位
+            new LinkedBlockingQueue<>(10000),  // 阻塞队列容量
+            new ThreadPoolExecutor.CallerRunsPolicy() // 拒绝策略：由调用线程处理任务
+    );
+
     // 全局线程池
     private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
 
@@ -29,7 +49,7 @@ public class CounterManager {
     //@NacosValue(value = "${timeInterval}", autoRefreshed = true)
 
 
-    private Cache<String, Integer> counterCache = Caffeine.newBuilder()
+    private final Cache<String, Integer> counterCache = Caffeine.newBuilder()
             .expireAfterWrite(1, TimeUnit.MINUTES)
             .build();
 
@@ -109,47 +129,21 @@ public class CounterManager {
         }
 
         String redisKey = key + ":" + timeFactor;
-        // Lua 脚本
-//        String luaScript =
-//                "if redis.call('exists', KEYS[1]) == 1 then " +
-//                        "  return redis.call('incr', KEYS[1]); " +
-//                        "else " +
-//                        "  redis.call('set', KEYS[1], 1); " +
-//                        "  redis.call('expire', KEYS[1], ARGV[1]); " +
-//                        "  return 1; " +
-//                        "end";
-//
-//        // 执行 Lua 脚本
-//        RScript script = redissonClient.getScript(IntegerCodec.INSTANCE);
-//        Object countObj = script.eval(
-//                RScript.Mode.READ_WRITE,
-//                luaScript,
-//                RScript.ReturnType.INTEGER,
-//                Collections.singletonList(key), expirationTimeInSeconds);
-//        return (long) countObj;
-//
-
-
 
         // 更新本地缓存中的计数
         counterCache.asMap().compute(redisKey, (k, v) -> v == null ? 1 : v + 1);
 
-        // 启动定时任务异步同步到 Redis
-        scheduleSyncToRedis();
-
         // 返回本地缓存中的计数值
         return redissonClient.getAtomicLong(redisKey).get();
     }
+
+    @PostConstruct
+    public void init() {
+        scheduleSyncToRedis();
+    }
     // 创建一个固定大小的线程池
     // 自定义线程池（IO 密集型线程池）
-    ThreadPoolExecutor customExecutor = new ThreadPoolExecutor(
-            2,             // 核心线程数
-            5,                        // 最大线程数
-            60L,                       // 线程空闲存活时间
-            TimeUnit.SECONDS,           // 存活时间单位
-            new LinkedBlockingQueue<>(10000),  // 阻塞队列容量
-            new ThreadPoolExecutor.CallerRunsPolicy() // 拒绝策略：由调用线程处理任务
-    );
+
     private void scheduleSyncToRedis() {
         scheduler.scheduleAtFixedRate(() -> {
             // 遍历 counterCache 中的所有键值对
@@ -158,13 +152,6 @@ public class CounterManager {
                 CompletableFuture.runAsync(() -> {
                     try {
                         // Lua 脚本：如果键存在则更新值，如果键不存在则设置值并设置过期时间
-                        String luaScript =
-                                "if redis.call('exists', KEYS[1]) == 1 then " +
-                                        "  redis.call('set', KEYS[1], ARGV[1]); " +  // Key 存在，只更新值
-                                        "else " +
-                                        "  redis.call('set', KEYS[1], ARGV[1], 'EX', ARGV[2]); " +  // Key 不存在，设置值和过期时间
-                                        "end " +
-                                        "return ARGV[1];";
 
                         // 获取 Redis 脚本对象
                         RScript script = redissonClient.getScript(IntegerCodec.INSTANCE);
@@ -172,7 +159,7 @@ public class CounterManager {
                         // 执行 Lua 脚本
                         script.eval(
                                 RScript.Mode.READ_WRITE,
-                                luaScript,
+                                LUA_SCRIPT,
                                 RScript.ReturnType.INTEGER,
                                 Collections.singletonList(key),  // 使用本地缓存的 key 作为 Redis 的 key
                                 value,  // ARGV[1]: 本地缓存中的值
@@ -189,43 +176,13 @@ public class CounterManager {
                     return null;
                 });
             });
-        }, 0, 15, TimeUnit.SECONDS); // 立即执行，每隔 15 秒执行一次
+        }, 0, 5, TimeUnit.SECONDS); // 立即执行，每隔 15 秒执行一次
+    }
+    @PreDestroy
+    public void destroy() {
+        customExecutor.shutdown();
+        scheduler.shutdown();
     }
 
-//    private void scheduleSyncToRedis(String redisKey, int count, CompletableFuture<Long> future) {
-//        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
-//        scheduler.scheduleAtFixedRate(() -> {
-//            try {
-//                String luaScript =
-//                        "if redis.call('exists', KEYS[1]) == 1 then " +
-//                                "  return redis.call('incrby', KEYS[1], ARGV[1]); " +
-//                                "else " +
-//                                "  redis.call('set', KEYS[1], ARGV[1]); " +
-//                                "  redis.call('expire', KEYS[1], ARGV[2]); " +
-//                                "  return ARGV[1]; " +
-//                                "end";
-//
-//                RScript script = redissonClient.getScript(IntegerCodec.INSTANCE);
-//                Object result = script.eval(
-//                        RScript.Mode.READ_WRITE,
-//                        luaScript,
-//                        RScript.ReturnType.INTEGER,
-//                        Collections.singletonList(redisKey),
-//                        count, // ARGV[1]: 增加的计数值
-//                        60     // ARGV[2]: 过期时间（秒）
-//                );
-//
-//                // 完成 CompletableFuture，返回 Redis 中的值
-//                future.complete((Long) result);
-//            } catch (Exception ex) {
-//                log.error("Failed to sync local cache to Redis for key: {}", redisKey, ex);
-//                // 完成 CompletableFuture，返回本地缓存中的值作为降级处理
-//                future.complete((long) count);
-//            } finally {
-//                // 清空当前键的本地缓存
-//                counterCache.invalidate(redisKey);
-//            }
-//        }, 0,5,TimeUnit.SECONDS); // 立即执行
-//    }
 
 }
